6b723d40b06a21d574a46f335914514e4e020b1b
Frander Mejia <frander.mejia15@gmail.com>
2024-05-05 10:16:03 -0600

    add new btns v6

--- webpack.config.js
+++ webpack.config.js
@@ -418,7 +418,7 @@ module.exports = async (env, argv) => {
     performance: {
       // Ignore media and sourcemaps when warning about file size.
       assetFilter(assetFilename) {
-        return !/\.(map|png|jpg|gif|glb|svg|webm)$/.test(assetFilename);
+        return !/\.(map|png|jpg|gif|glb|webm)$/.test(assetFilename);
       }
     },
     module: {
@@ -549,7 +549,7 @@ module.exports = async (env, argv) => {
           oneOf: [
             { resourceQuery: /inline/, type: "asset/inline" },
             {
-              test: /\.(png|jpg|gif|glb|ogg|mp3|mp4|wav|woff2|svg|webm|3dl|cube)$/,
+              test: /\.(png|jpg|gif|glb|ogg|mp3|mp4|wav|woff2|webm|3dl|cube)$/,
               type: "asset/resource",
               generator: {
                 // move required assets to output dir and add a hash for cache busting
