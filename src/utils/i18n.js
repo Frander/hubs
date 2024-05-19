b02eb75b9e788b647b54e1517695f08f803b1787
Frander Mejia <frander.mejia15@gmail.com>
2024-05-19 15:25:25 -0600

    fix translates 11

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -60,16 +60,17 @@ function findLocale(locale) {
 
 export function setLocale(locale) {
   console.log(locale)
-  const resolvedLocale = findLocale(locale);
+  //const resolvedLocale = findLocale(locale);
+  const resolvedLocale = es;
   //force "es"
-  resolvedLocale = "es";
+  //resolvedLocale = "es";
   console.log(resolvedLocale)
   console.log(DEFAULT_LOCALE)
   if (resolvedLocale === DEFAULT_LOCALE) {
-    // _locale = resolvedLocale;
-    // _localeData = defaultLocaleData;
-    // console.log("DEFAULT_LOCALE")
-    // window.dispatchEvent(new CustomEvent("locale-updated"));
+    _locale = resolvedLocale;
+    _localeData = defaultLocaleData;
+    console.log("DEFAULT_LOCALE")
+    window.dispatchEvent(new CustomEvent("locale-updated"));
     // import(`../assets/locales/${resolvedLocale}.json`).then(({ default: localeData }) => {
     //   console.log(resolvedLocale)
     //   console.log(localeData)
