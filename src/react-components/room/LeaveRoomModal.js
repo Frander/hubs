afd4a64b1c562c7ac6c5cbcebf639b7d3f9a1f57
Frander Mejia <frander.mejia15@gmail.com>
2024-12-11 23:23:31 -0600

    Fix: url landing

--- src/react-components/room/LeaveRoomModal.js
+++ src/react-components/room/LeaveRoomModal.js
@@ -52,7 +52,7 @@ export function LeaveRoomModal({ reason, destinationUrl, onClose }) {
     >
       <Column padding center centerMd="both" grow>
         <p>{intl.formatMessage(reasonMessages[reason])}</p>
-        <Button as="a" preset="cancel" href={destinationUrl} rel="noopener noreferrer">
+        <Button as="a" preset="cancel" href="https://spacemall.es" rel="noopener noreferrer">
           {intl.formatMessage(confirmationMessages[reason])}
         </Button>
       </Column>
