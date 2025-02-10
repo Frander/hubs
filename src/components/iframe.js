110830833b207117489c40ba8b24c9016467744a
Frander Mejia <frander.mejia15@gmail.com>
2025-02-09 23:21:17 -0600

    fix iframe click #3

--- src/components/iframe.js
+++ src/components/iframe.js
@@ -99,6 +99,7 @@ AFRAME.registerComponent("iframe", {
 
     const mouse = new THREE.Vector2();
     const raycaster = new THREE.Raycaster();
+    const camera = this.el.cameraEl.object3D;
 
     // Click event listener
     this.el.sceneEl.renderer.domElement.addEventListener("click", (event) => {
